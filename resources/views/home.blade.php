b7bf6098973be6301a42f3707f631c4e7ceda18a
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-11-02 14:28:22 +0700

    modified:   resources/views/home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -128,20 +128,6 @@
           <a href="/products" class="small-box-footer">Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
         </div>
       </div>
-      <div class="col-lg-3 col-6">
-        <!-- small box -->
-        <div class="small-box bg-warning">
-          <div class="inner">
-              <h4>{{config('settings.currency_symbol')}} {{number_format($outcome, 2)}}</h4>
-            <p>Outcome</p>
-          </div>
-          <div class="icon">
-            <i class="ion ion-stats-bars"></i>
-          </div>
-          <a href="#" class="small-box-footer">Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-      </div>
-      <!-- ./col -->
 </div>
 @endif
 
